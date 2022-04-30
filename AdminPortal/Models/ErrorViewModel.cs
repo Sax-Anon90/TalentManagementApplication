3d7f145891696a1287225e140f78bccbd84116b5
Sax-Anon <olwethumkize15@gmail.com>
2022-04-30 14:03:59 +0200

    Add project files.

---
+++ AdminPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdminPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
